444cf7189d0eb2af9f18e4a7f36bae48a304dc2c
ymd <129515331+jxufeDD@users.noreply.github.com>
2023-09-05 16:19:05 +0800

    spring 的入门

---
+++ src/spring/MainTest2.java
@@ -0,0 +1,15 @@
+package spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring.dao.BookDao;
+
+public class MainTest2 {
+    public static void main(String[] args) {
+        //获取ioc容器
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        //获取bean
+        BookDao bookDao = (BookDao) context.getBean("bookDao");
+        bookDao.bookDao();
+    }
+}
